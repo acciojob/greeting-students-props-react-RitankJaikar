34eaaf67a555b5a81a7c601d9e1c29228ca85671
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-25 11:54:51 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  // You can set the name dynamically or statically
+  const [name, setName] = useState("John");
+
   return (
     <Welcome name="" />
   )
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+
+// Functional component to display the welcome message
+const Welcome = ({ name }) => {
+  return (
+    <div>
+      <h1>Hey {name}!</h1> {/* Greeting message with name */}
+      <h2>Welcome to School.</h2> {/* Welcome message */}
+    </div>
+  );
+};
+
+export default Welcome;
